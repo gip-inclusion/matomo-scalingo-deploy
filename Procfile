379f2013063d891859f12b7c45e2c726150d79e5
Jérémy Buget <jbuget@gmail.com>
2025-05-07 17:15:10 +0200

    Use custom clock process instead of scalingo scheduler

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bin/start-matomo.sh
-cron: bin/auto-archiving-reports.sh
+clock: php cron.php
---
+++ cron.php
@@ -0,0 +1,45 @@
+<?php
+require(__DIR__ . '/vendor/autoload.php');
+
+echo "[CRON] Starting tasks scheduler\n";
+
+use Cron\Job\ShellJob;
+use Cron\Schedule\CrontabSchedule;
+use Cron\Resolver\ArrayResolver;
+use Cron\Cron;
+use Cron\Executor\Executor;
+
+function build_cron() {
+    $job = new ShellJob();
+    $job->setCommand('bash bin/auto-archiving-reports.sh');
+    // Toutes les 4 heures
+    $job->setSchedule(new CrontabSchedule('0 */4 * * *'));
+
+    $resolver = new ArrayResolver();
+    $resolver->addJob($job);
+
+    $cron = new Cron();
+    $cron->setExecutor(new Executor());
+    $cron->setResolver($resolver);
+
+    return $cron;
+}
+
+$cron = build_cron();
+
+while (true) {
+    echo "[CRON] Running tasks\n";
+    $report = $cron->run();
+
+    while ($cron->isRunning()) { }
+
+    echo "[CRON] " . count($report->getReports()) . " tasks have been executed\n";
+
+    foreach ($report->getReports() as $job_report) {
+        foreach ($job_report->getOutput() as $line) {
+            echo "[CRON] " . $line . "\n";
+        }
+    }
+
+    sleep(60);
+}
