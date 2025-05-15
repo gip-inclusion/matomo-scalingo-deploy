933066153ac32a3c48999f5875f6d7e1b9dc48ed
Jérémy Buget <jbuget@gmail.com>
2025-05-15 08:07:35 +0200

    cron: declare only one job

--- bin/cron-runner.sh
+++ bin/cron-runner.sh
@@ -9,9 +9,7 @@ bin/configure-environment.sh
 # Format: minute hour day month weekday
 JOBS=(
   "*/15 * * * * echo 'CRON runner is up and running.'"
-  "5 */2 * * * php console core:archive --force-idsites 117,136,146,214 --skip-segments-today --concurrent-requests-per-website 10 --concurrent-archivers 3"
-  "10 */2 * * * php console core:archive --force-idsites 206,211,221 --skip-segments-today --concurrent-requests-per-website 10 --concurrent-archivers 3"
-  "15 */2 * * * php console core:archive --force-idsites 207,217,212 --skip-segments-today --concurrent-requests-per-website 10 --concurrent-archivers 3"
+  "5 */4 * * * php console core:archive --skip-segments-today --concurrent-requests-per-website 5"
   "0 4 * * * php console core:purge-old-archive-data all && php console database:optimize-archive-tables all"
 )
 
