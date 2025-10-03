bdb65a6fb319ac7e65da7e0a0119992020b6ab81
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-10-03 15:34:44 +0200

    cron-runner: Try running core:archive every hour

--- bin/cron-runner.sh
+++ bin/cron-runner.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 echo "Launching CRON runner..."
 
@@ -9,7 +9,7 @@ bin/configure-environment.sh
 # Format: minute hour day month weekday
 JOBS=(
   "*/15 * * * * echo 'CRON runner is up and running.'"
-  "5 */4 * * * php console core:archive --skip-segments-today --concurrent-requests-per-website 8"
+  "5 * * * * php console core:archive --skip-segments-today --concurrent-requests-per-website 8"
   "0 4 * * * php console core:purge-old-archive-data all && php console database:optimize-archive-tables all"
 )
 
