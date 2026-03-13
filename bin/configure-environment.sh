e7f888291d30d98c3b2734dace8e15cb3bb06810
Victor Perron <victor@iso3103.net>
2026-03-13 09:44:31 +0100

    chore: fix start order of commands

--- bin/configure-environment.sh
+++ bin/configure-environment.sh
@@ -2,9 +2,13 @@
 
 echo "Configuring environment..."
 
+bin/generate-config-ini.sh
+
 bin/fetch-purchased-plugins.sh
 
-bin/generate-config-ini.sh
+php console core:clear-caches
+
+php console core:update --yes
 
 php console custom-matomo-js:update
 
