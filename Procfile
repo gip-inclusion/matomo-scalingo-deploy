7a847180b6c0f4b902e830db9c99c90a56ffdbfd
Victor Perron <victor@iso3103.net>
2026-03-16 23:04:37 +0100

    chore: optimize crons & deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bin/start-matomo.sh
+postdeploy: bin/configure-environment.sh && php console core:clear-caches && php console core:update --yes
--- bin/configure-environment.sh
+++ bin/configure-environment.sh
@@ -6,9 +6,5 @@ bin/fetch-purchased-plugins.sh
 
 bin/generate-config-ini.sh
 
-php console core:clear-caches
-
-php console core:update --yes
-
 php console custom-matomo-js:update
 
