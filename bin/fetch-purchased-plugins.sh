a65e770588ba1926710297ad503e4cef1f457ee1
Victor Perron <victor@iso3103.net>
2026-09-01 21:11:28 +0200

    chore: refactor plugin fetch script to pin versions in repo

--- bin/fetch-purchased-plugins.sh
+++ bin/fetch-purchased-plugins.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
-echo "Fetching Matomo purchased plugins..."
+PURCHASED_PLUGINS=(
+  "QueuedTracking:5.2.0"
+  "HeatmapSessionRecording:5.3.3"
+  "FormAnalytics:5.1.3"
+  "AbTesting:5.5.1"
+  "MediaAnalytics:5.0.13"
+  "Funnels:5.3.12"
+)
+
+FREE_PLUGINS=(
+)
+
+download_plugin() {
+  local plugin_name=$1
+  local plugin_version=$2
+  local access_token=$3
+  local plugins_dir=$4
+  local zip_file_location="$plugins_dir/$plugin_name.zip"
+  local url="https://plugins.matomo.org/api/2.0/plugins/$plugin_name/download/$plugin_version"
+
+  rm -rf "$plugins_dir/$plugin_name"
+  rm -f "$zip_file_location"
+  echo "Downloading plugin $plugin_name#$plugin_version at $url"
+  if [[ -n "$access_token" ]]; then
+    curl -s -X POST -F "access_token=$access_token" -L "${url}" -o "$zip_file_location"
+  else
+    curl -s -L "${url}" -o "$zip_file_location"
+  fi
+
+  echo "Unzipping $zip_file_location to $plugins_dir/$plugin_name"
+  unzip -qo "$zip_file_location" -d "$plugins_dir"
+}
+
+echo "Fetching Matomo plugins..."
+
+plugins_dir="./plugins"
+mkdir -p "${PWD}/$plugins_dir"
+
+for plugin in "${FREE_PLUGINS[@]}"; do
+  download_plugin "${plugin%%:*}" "${plugin##*:}" "" "$plugins_dir"
+done
 
 if [[ -z "$MATOMO_LICENSE_KEY" ]]; then
-  echo "Do not fetch purchased plugins because no Matomo plugins API license key defined"
+  echo "Skipping purchased plugins: no MATOMO_LICENSE_KEY defined"
 else
-  if [[ -z "$MATOMO_PURCHASED_PLUGINS" ]]; then
-    echo "No purchased plugins specified. Did you forgotten to set MATOMO_PURCHASED_PLUGINS variable?"
-  else
-    plugins_dir="./plugins"
-
-    mkdir -p "${PWD}/$plugins_dir"
-
-    IFS=',' read -ra plugins <<<"$MATOMO_PURCHASED_PLUGINS"
-    for plugin in "${plugins[@]}"; do
-      plugin_name=${plugin%%:*}
-      plugin_version=${plugin##*:}
-
-      access_token="$MATOMO_LICENSE_KEY"
-      zip_file_name="$plugin_name.zip"
-      zip_file_location="$plugins_dir/$zip_file_name"
-      url="https://plugins.matomo.org/api/2.0/plugins/$plugin_name/download/$plugin_version"
-
-      echo "Downloading plugin $plugin_name#$plugin_version at $url to $zip_file_location"
-      if curl -s -X POST -F "access_token=$access_token" -L "${url}" -o "$zip_file_location"; then
-
-        echo "Unzipping $zip_file_location to $plugins_dir/$plugin_name"
-        unzip -q "$zip_file_location" -d "$plugins_dir"
-
-        if [[ -f "$zip_file_location" ]]; then
-          echo "Removing $zip_file_location"
-          rm -f "$zip_file_location"
-        fi
-      else
-        echo "Something went wrong"
-      fi
-    done
-  fi
+  for plugin in "${PURCHASED_PLUGINS[@]}"; do
+    download_plugin "${plugin%%:*}" "${plugin##*:}" "$MATOMO_LICENSE_KEY" "$plugins_dir"
+  done
 fi
