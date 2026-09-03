60be5d791461e50e345a4747abb87614f25962cf
Victor Perron <victor@iso3103.net>
2026-09-03 12:06:19 +0200

    chore: rationalize setup & launch scripts

--- bin/first-deploy-init.sh
+++ bin/first-deploy-init.sh
@@ -2,9 +2,6 @@
 
 echo "Initializing Matomo database and users..."
 
-bin/generate-config-ini.sh
-
-php console database:create-tables && \
-  php console core:update --yes && \
+bin/configure-environment.sh && \
   php console admin:create-superuser --login $MATOMO_INIT_USER_LOGIN --email $MATOMO_INIT_USER_EMAIL --password $MATOMO_INIT_USER_PASSWORD && \
   php console admin:create-site --name $MATOMO_INIT_SITE_NAME --url $MATOMO_INIT_SITE_URL
